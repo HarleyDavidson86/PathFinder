ad699fcd5a1dd5c4a27f9d9c37b39758b5d715d6
Dominik Sust <dominik.sust@gmail.com>
2024-09-24 16:17:16 +0200

    Prepared next case to implement

--- src/main/java/de/itwerkstatt/pathfinder/GUITest.java
+++ src/main/java/de/itwerkstatt/pathfinder/GUITest.java
@@ -12,10 +12,22 @@ import java.io.IOException;
  */
 public class GUITest {
     public static void main(String[] args) throws InterruptedException, IOException {
+        // (100,100)  (200,100) (300,100)  (400,100)
+        //     o----------o         o----------o
+        //     |          |         |          |
+        //     |          |         |          |
+        //     |     s    |         |    e     |
+        //     | (150,160)|         |(350,160) |
+        //     |          o---------o          |
+        //     |      (200,200) (300,200)      |
+        //     |                               |
+        //     o-------------------------------o
+        // (100,300)                       (400,300)
         PathFinder p = new PathFinder(new Area(
                 new Point(100, 100),
                 new Point(200, 100),
-                new Point(250, 200),
+                new Point(200, 200),
+                new Point(300, 200),
                 new Point(300, 100),
                 new Point(400, 100),
                 new Point(400, 300),
@@ -23,10 +35,11 @@ public class GUITest {
         ));
         p.setAreaTriangles(
                 new Triangle(new Point(100, 300), new Point(100, 100), new Point(200, 100)),
-                new Triangle(new Point(100, 300), new Point(200, 100), new Point(250, 200)),
-                new Triangle(new Point(100, 300), new Point(250, 200), new Point(400, 300)),
-                new Triangle(new Point(400, 300), new Point(250, 200), new Point(300, 100)),
-                new Triangle(new Point(400, 300), new Point(300, 100), new Point(400, 100))
+                new Triangle(new Point(100, 300), new Point(200, 100), new Point(200, 200)),
+                new Triangle(new Point(100, 300), new Point(200, 200), new Point(300, 200)),
+                new Triangle(new Point(100, 300), new Point(300, 200), new Point(400, 300)),
+                new Triangle(new Point(300, 200), new Point(300, 100), new Point(400, 100)),
+                new Triangle(new Point(300, 200), new Point(400, 100), new Point(400, 300))
         );
         Point s = new Point(150,160);
         Point d = new Point(350,160);
