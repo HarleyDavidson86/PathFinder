2baa50a1cce41e7877780fda5364990240e61461
Dominik Sust <dominik.sust@gmail.com>
2024-10-02 11:21:47 +0200

    Setup new testcase

--- src/main/java/de/itwerkstatt/pathfinder/GUITest.java
+++ src/main/java/de/itwerkstatt/pathfinder/GUITest.java
@@ -13,45 +13,45 @@ import java.io.IOException;
 public class GUITest {
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        // (100,100) (200,100) (300,100)                     (600,100)
-        //     o---------o         o-----------------------------o
-        //     |         |         |                             |
-        //     |    s    |         |     (400,200) (500,200)     |
-        //     |(150,150)|         |         o---------o         |
-        //     |         |         |         |         |         |
-        //     |         o---------o         |         |         |
-        //     |     (200,400) (300,400)     |         |    e    |
-        //     |                             |         |(550,450)|
-        //     o-----------------------------o         o---------o
-        // (100,500)                     (400,500) (500,500) (600,500)
+        // (100,100)    (250,100)          (500,100)
+        //     o------o     o------------------o
+        //     |      |     |     (400,200)    |
+        //     |      |     |    o----o        |
+        //     |   s  |     | e  |    |        |
+        //     |      |     o----o    |        |
+        //     |      |               |        |
+        //     |      o---------------o        |
+        //     |  (200,400)       (400,400)    |
+        //     o-------------------------------o
+        // (100,500)                       (500,500)
         PathFinder p = new PathFinder(new Area(
                 new Point(100, 100),
                 new Point(200, 100),
                 new Point(200, 400),
-                new Point(300, 400),
-                new Point(300, 100),
-                new Point(600, 100),
-                new Point(600, 500),
-                new Point(500, 500),
-                new Point(500, 200),
+                new Point(400, 400),
                 new Point(400, 200),
-                new Point(400, 500),
+                new Point(350, 200),
+                new Point(350, 350),
+                new Point(250, 350),
+                new Point(250, 100),
+                new Point(500, 100),
+                new Point(500, 500),
                 new Point(100, 500)
         ));
         p.setAreaTriangles(
-                new Triangle(new Point(100, 500), new Point(100, 100), new Point(200, 100)),
-                new Triangle(new Point(100, 500), new Point(200, 100), new Point(200, 400)),
-                new Triangle(new Point(100, 500), new Point(200, 400), new Point(300, 400)),
-                new Triangle(new Point(100, 500), new Point(300, 400), new Point(400, 500)),
-                new Triangle(new Point(300, 400), new Point(300, 100), new Point(400, 200)),
-                new Triangle(new Point(300, 400), new Point(400, 200), new Point(400, 500)),
-                new Triangle(new Point(300, 100), new Point(600, 100), new Point(500, 200)),
-                new Triangle(new Point(300, 100), new Point(500, 200), new Point(400, 200)),
-                new Triangle(new Point(600, 100), new Point(600, 500), new Point(500, 500)),
-                new Triangle(new Point(600, 100), new Point(500, 500), new Point(500, 200))
+                new Triangle(new Point(100, 100), new Point(200, 100), new Point(200, 400)),
+                new Triangle(new Point(100, 100), new Point(200, 400), new Point(100, 500)),
+                new Triangle(new Point(100, 500), new Point(200, 400), new Point(400, 400)),
+                new Triangle(new Point(100, 500), new Point(400, 400), new Point(500, 500)),
+                new Triangle(new Point(500, 500), new Point(400, 400), new Point(400, 200)),
+                new Triangle(new Point(500, 500), new Point(400, 200), new Point(500, 100)),
+                new Triangle(new Point(500, 100), new Point(400, 200), new Point(350, 200)),
+                new Triangle(new Point(500, 100), new Point(350, 200), new Point(250, 100)),
+                new Triangle(new Point(250, 100), new Point(350, 200), new Point(350, 350)),
+                new Triangle(new Point(250, 100), new Point(350, 350), new Point(250, 350))
         );
         Point s = new Point(150, 150);
-        Point d = new Point(550, 450);
+        Point d = new Point(300, 300);
         p.setStartAndEndpoint(s, d);
         new TestGUI(p);
     }
